c9f809aa388b838b71d8868a4631ab67650e427c
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-03 01:47:28 +0700

    del input masks

--- src/blocks/input-text/input-text.js
+++
@@ -1,6 +0,0 @@
-import 'jquery-mask-plugin';
-
-$(function() {
-  $("input[name='date']").mask("99.99.9999");
-});
-
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import './blocks/link/link.scss'
 /*blocks*/
 import './blocks/checkbox-list/checkbox-list.scss'
 import './blocks/input-text/input-text.scss'
-import './blocks/input-text/input-text.js'
 import './blocks/radiogroup/radiogroup.scss'
 import './blocks/range-slider/range-slider.scss'
 import './blocks/topic-label/topic-label.scss'
