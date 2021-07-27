64f0d0bc139c84f024f19d157477899083ffbe47
KsuVla <ksu.xa.v.ya@gmail.com>
2021-07-28 03:36:25 +0700

    fixed webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,84 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const devserver = require('./webpack/devserver');
 const pug = require('./webpack/pug');
 const html = require('./webpack/html');
 const sass = require('./webpack/sass');
-const css = require('./webpack/css');
-
 const images = require('./webpack/images');
 const font = require('./webpack/font');
 const ts = require('./webpack/typescript');
 
-
-const PATHS = {
+const paths = {
   source: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'docs')
+  build: path.join(__dirname, 'docs'),
 };
 
-
-const PLUGINSdate = [
+const pluginsDate = [
   new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery'
+    jQuery: 'jquery',
   }),
   new HtmlWebpackPlugin({
     title: 'Webpack app',
     filename: 'index.html',
     chunks: ['index'],
-    template: PATHS.source + '/index.pug'
-  })
+    template: paths.source + '/index.pug',
+  }),
 ];
 
-const ENTRYdate = {
-  'index.js': PATHS.source + '/index.js',
+const entryDate = {
+  'layout.js': paths.source + '/blocks/layout/layout.js',
+  'page-demo-sliders.js': paths.source + '/page-demo-sliders/page-demo-sliders.js',
 };
-
 
 const common = merge([
   {
     mode: 'production',
     devtool: 'inline-source-map',
-    entry: ENTRYdate,
+    entry: entryDate,
     output: {
-      path: PATHS.build,
-      filename: '[name]'
+      path: paths.build,
+      filename: 'js/[name]',
     },
     resolve: {
       alias: {
         '@Blocks': path.resolve(__dirname, 'src/blocks/'),
-        '@Pages': path.resolve(__dirname, 'src/pages/'),
         '@': path.resolve(__dirname, 'src/'),
       },
     },
-    plugins: PLUGINSdate,
+    plugins: pluginsDate,
   },
 
   pug(),
   html(),
   sass(),
-  css(),
   font(),
   images(),
   ts(),
 ]);
 
-
 const developmentConfig = {
-	mode: 'development',
+  mode: 'development',
   devServer: {
     stats: 'errors-only',
-    port: 9000
+    port: 9001,
   },
-  
 };
 
 const productionConfig = {
   mode: 'production',
-  plugins: [
-    new CleanWebpackPlugin(),
-  ]
-}
+  plugins: [new CleanWebpackPlugin()],
+};
 
-
-module.exports = function(env) {
-
+module.exports = function (env) {
   if (env.conf === 'development') {
-    return merge([
-      common,
-      developmentConfig,
-    ])
+    return merge([common, developmentConfig]);
   }
-  
+
   if (env.conf === 'production') {
-    return merge([
-      common,
-      productionConfig,
-    ])
+    return merge([common, productionConfig]);
   }
 };
